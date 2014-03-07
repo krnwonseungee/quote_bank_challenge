940d0b0d2b820eb6867ef45156bcc715bf934ac0
Janet Yi <janetyi90@gmail.com>
2014-03-07 15:45:02 -0800

    Update quote_view.rb

--- app/views/quote_view.rb
+++ app/views/quote_view.rb
@@ -1,48 +1,45 @@
 class QuoteView
   class << self
-  def intro_list_commands
-    puts "Welcome to Quot.ly! a random quote to lighten up your day :)"
-    puts "Pick an option:"
-    puts "ADD"
-    puts "DELETE"
-    puts "RANDOM"
-    puts "LIST"
-    puts "QUIT"
-  end
-
-  def return_options_screen
-    puts "Pick an option:"
-    puts "ADD"
-    puts "DELETE"
-    puts "RANDOM"
-    puts "LIST"
-    puts "QUIT"
-  end
-  
-  def error_screen
-    puts "Please select a valid option."
-  end
-  
-  def display(ar_quote_obj)
-    ar_quote_obj.each do |quote_obj|
-      puts "#{quote_obj.message} - #{quote_obj.author}"
+    def intro_list_commands
+      puts "Welcome to Quot.ly! a random quote to lighten up your day :)"
+      puts "Pick an option:"
+      puts "ADD"
+      puts "DELETE"
+      puts "RANDOM"
+      puts "LIST"
+      puts "QUIT"
     end
-  end
 
-  def random_display(quote_obj)
-    puts "#{quote_obj.text}  - #{quote_obj.author}"
-  end
+    def return_options_screen
+      puts "Pick an option:"
+      puts "ADD"
+      puts "DELETE"
+      puts "RANDOM"
+      puts "LIST"
+      puts "QUIT"
+    end
 
-  def add_quote_screen
-    puts "Your quote has been added!"
-  end
+    def error_screen
+      puts "Please select a valid option."
+    end
 
-  def delete_quote_screen
-    puts "Your quote has been deleted."
-  end
+    def display(ar_quote_obj)
+      ar_quote_obj.each do |quote_obj|
+        puts "#{quote_obj.message} - #{quote_obj.author}"
+      end
+    end
 
-  def exit_screen
-    puts "Have an enlightening day!"
+    def add_quote_screen
+      puts "Your quote has been added!"
+    end
+
+    def delete_quote_screen
+      puts "Your quote has been deleted."
+    end
+
+    def exit_screen
+      puts "Have an enlightening day!"
+    end
   end
 end
 
