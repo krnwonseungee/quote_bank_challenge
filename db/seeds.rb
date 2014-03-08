a76e983460b714e5aa94047f98bf953147fc4b2e
Janet Yi <janetyi90@gmail.com>
2014-03-07 16:57:12 -0800

    Update seeds.rb
    updated seeds.rb with quotes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,22 @@
+dbc_quote1 = Quote.create(author: "Duke Ellington", text: "Fate is being kind to me, fate doesn't want me to be too famous too young.")
+dbc_quote2 = Quote.create(author: "African Proverb", text: "If you want to go fast, go alone. If you want to go far, go together.")
+dbc_quote3 = Quote.create(author: "Jedi Master Yoda", text: "Do or do not there is not try")
+dbc_quote4 = Quote.create(author: "David Whyte", text: "Anything or anyone that does not bring you alive is to small for you.")
+dbc_quote5 = Quote.create(author: "Earl Nig", text: "Luck is when preparedness meets opportunity, and opportunity is everywhere.")
+dbc_quote6 = Quote.create(author: "Daniel Burnham", text: "Make no little plans; The have no magic to stir mens blood.")
 
 
-# weekend = List.create(title: "Weekend")
-# weekdays = List.create(title: "Weekdays")
+Category.create(name: "DBC")
 
-# weekend.tasks << Task.create(message: "Walk the dog!")
-# weekend.tasks << Task.create(message: "Go to the store")
-# weekdays.tasks << Task.create(message: "Clean the house")
-# weekdays.tasks << Task.create(message: "Call my mom and your mom")
-# weekdays.tasks << Task.create(message: "Take a shower")
-# weekend = List.new(title: "Weekend")
-# task = Task.new(message: "walk dog")
-# weekend.tasks << task
-# task.save
-# weekend.save => lines 11 through 15 works
-my_quote = Quote.new author: "Duke Ellington", text: "Fate is being kind to me, fate doesn't want me to be too famous too young."
+Category.find(1).quotes << Quote.find(1)
+Category.find(1).quotes << Quote.find(2)
+Category.find(1).quotes << Quote.find(3)
+Category.find(1).quotes << Quote.find(4)
+Category.find(1).quotes << Quote.find(5)
+Category.find(1).quotes << Quote.find(6)
 
-
-
-
-# List.new(title: "Weekend").save
-# Task.new(message: "walk dog").save
-
-# List.find(1) << Task.find(1)
+6.times do |i|
+  this_quote = Quote.find(i+1)
+  puts "Quote: #{this_quote.text} \nBy #{this_quote.author}"
+  puts
+end
